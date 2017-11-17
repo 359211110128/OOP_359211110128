7949f9b861c0757947bb393ef5e1ff93f44eeb00
LABCOM_6 <suraidah218@gmail.com>
2017-11-17 11:14:33 +0700

    $sucessfuly information data

--- src/oop_lab/s359211110128/rmutvs/com/myfirstJAVA.java
+++ src/oop_lab/s359211110128/rmutvs/com/myfirstJAVA.java
@@ -4,6 +4,9 @@ public class myfirstJAVA {
 
     public static void main(String[] args) {
         System.out.println("Hello Word, I'm DA");
+        System.out.println("Information System");
+        System.out.println("Faculty of Managrment Technology");
+        System.out.println("RMUTSV");
     }// main
 
 
