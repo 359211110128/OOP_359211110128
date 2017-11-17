cfe5f2d79808d96f7d87ac8ceab6c0696032f622
LABCOM_6 <suraidah218@gmail.com>
2017-11-17 11:19:24 +0700

    testing edit code

--- src/oop_lab/s359211110128/rmutvs/com/myfirstJAVA.java
+++ src/oop_lab/s359211110128/rmutvs/com/myfirstJAVA.java
@@ -5,8 +5,7 @@ public class myfirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Word, I'm DA");
         System.out.println("Information System");
-        System.out.println("Faculty of Managrment Technology");
-        System.out.println("RMUTSV");
+
     }// main
 
 
