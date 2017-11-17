6c6a0d18061c2ed476b0fca5242bb093f8f4224f
unknown <suraidah218@gmail.com>
2017-11-17 10:58:02 +0700

    Initial commit

---
+++ src/oop_lab/s359211110128/rmutvs/com/myfirstJAVA.java
@@ -0,0 +1,11 @@
+package oop_lab.s359211110128.rmutvs.com;
+
+public class myfirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word, I'm DA");
+    }// main
+
+
+
+}// class
