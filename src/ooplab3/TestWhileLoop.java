372151553d8852284ab9bc1338713ac8e3926664
LAB_COM-MT_1 <suraidah218@gmail.com>
2017-12-11 15:52:12 +0700

    TestWhileLoop1

--- src/ooplab3/TestWhileLoop.java
+++ src/ooplab3/TestWhileLoop.java
@@ -2,22 +2,23 @@ package ooplab3;
 
 public class TestWhileLoop {
     public static void main(String[] args) {
-        //whileloop
-        int i=1 ;
-        while (i<10){
-            if (i % 3 == 0 || i % 5 == 0 )
-            System.out.println(i+"");
+        //while
+        int i = 1;
+        while (i++<=100) {
+            if (i%3==0&&i%5==0)
+                System.out.print(i+" ");
             i++;
 
         }//while
+//      do-while
         System.out.println();
-        int j=i;
+        int j = 1;
         do {
             if (i % 3 == 0 && 5 == 0)
-                System.out.println(j + "'");
+                System.out.print(j + " ");
         }while (++j<=100);
-        }
     }
 
+}
 
 
