ec66337d1fd84bd37a422e6fbf1b94e0d06ef4da
359211110128 <suraidah218@gmail.com>
2017-12-29 09:56:09 +0700

    StringCompare false 1

--- src/ooplab6/myStrBuff.java
+++ src/ooplab6/myStrBuff.java
@@ -5,11 +5,14 @@ public class myStrBuff {
         //StringBuffer
         StringBuffer myBuf = new StringBuffer();
         myBuf.append("Hello");
-        myBuf.append(" Welcome ");
-        myBuf.append(" RMUTSV ");
-        myBuf.append(2017);
+        myBuf.append(" " + "Welcome");
+        myBuf.append(" " + "RMUTSV");
+        myBuf.append(" " + 2017);
         System.out.println(myBuf);
-
+        myBuf.insert(14,"MT ");
+        System.out.println(myBuf);
+        System.out.println(myBuf.length());
+        System.out.println(myBuf.capacity());
 
 
 
