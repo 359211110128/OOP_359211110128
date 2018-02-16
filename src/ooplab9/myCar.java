3b447eb31274cbaf514396e47527aa303852ed29
LAB_COM-MT_1 <suraidah218@gmail.com>
2018-02-16 10:06:45 +0700

    Engine

--- src/ooplab9/myCar.java
+++ src/ooplab9/myCar.java
@@ -1,12 +1,13 @@
-package oop_lab9;
+package ooplab9;
 
 public class myCar {
     public static void main(String[] args) {
         Engine engine = new Engine("1500 cc","V-TEC V4");
-
-        Car car = new Car("Honda","Red",engine);
+        Car car =new Car("Honda","Red",engine);
         System.out.println(car.toString());
 
 
-    }//main
+    }
+
+
 }//class
